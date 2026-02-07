c29b0dd014abf8a4260f7a7502d9fa421ecca7c6
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2026-02-07 12:27:53 -0500

    Changing regex for openproblems

--- problem-scraper/src/scraper/patterns.py
+++ problem-scraper/src/scraper/patterns.py
@@ -1,2 +1,2 @@
 conjecture = r"\\begin{conj}(\\label{(.*)})?([\s\S]*?)\\end{conj}"
-openproblem = r"\\begin{openproblem}\{(.*)?\}\{(.*)?\}\{(.*)?\}([\s\S]*?)\\end{openproblem}"
+openproblem = r"\\openproblem\{(.*)?\}\{(.*)?\}\{(.*)?\}[\s]*\{(.*)?\}"
